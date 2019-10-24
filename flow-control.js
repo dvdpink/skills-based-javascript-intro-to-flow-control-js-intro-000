7dd457d780b63a0c1829530435fe88da78eb0795
David Pink <dvdpink@gmail.com>
2019-10-24 15:55:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,8 +17,7 @@ function ageChecker(age) {if (age >=13 && age <=19){return "You are a teenager!"
 
 }
 
-function ternaryTeenager(age) {age <=13 && age <=19) ?
-  "You are a teenager":"You are not a teenager"
+function ternaryTeenager(age) {
 
 }
 
